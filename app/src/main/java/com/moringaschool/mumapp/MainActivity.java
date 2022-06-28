b546888510f7758dd70acade50c8b9c0e231f840
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2022-06-28 23:16:59 +0300

    This will add the first commit

---
+++ app/src/main/java/com/moringaschool/mumapp/MainActivity.java
@@ -0,0 +1,14 @@
+git addgpackage com.moringaschool.mumapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
