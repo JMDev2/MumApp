f65dee712cc8a09563b7bdb3ea716d6b4e686435
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2022-07-06 15:52:16 +0300

    adding uncommited changes

--- app/src/main/java/com/moringaschool/mumapp/ui/IntroActivity.java
+++ app/src/main/java/com/moringaschool/mumapp/ui/IntroActivity.java
@@ -4,13 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+
 
 import com.moringaschool.mumapp.R;
 
-import butterknife.BindView;
+
 
 public class IntroActivity extends AppCompatActivity {
 
