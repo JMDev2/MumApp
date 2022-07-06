6b085be1431313f435d8ae273faf7e6aacb8aea9
Sylvia-Chepkoech <slycheche@gmail.com>
2022-07-06 14:13:54 +0300

    refactor code

--- app/src/main/java/com/moringaschool/mumapp/ui/IntroActivity.java
+++ app/src/main/java/com/moringaschool/mumapp/ui/IntroActivity.java
@@ -12,39 +12,14 @@ import com.moringaschool.mumapp.R;
 
 import butterknife.BindView;
 
-public class IntroActivity extends AppCompatActivity implements View.OnClickListener {
-//    @BindView(R.id.signup)
-//    Button mButton;
-//    @BindView(R.id.account)
-//    TextView mText;
-    private Button mButton;
-    private TextView mText;
+public class IntroActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
 
-        mButton = findViewById(R.id.signup);
-        mText = findViewById(R.id.account);
-
-
-        mButton.setOnClickListener(this);
-        mText.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-        if (v == mButton) {
-            Intent intent = new Intent(IntroActivity.this, StatusActivity.class);
-            startActivity(intent);
-            finish();
-        }
-
-        if (v == mText) {
-            Intent intent = new Intent(IntroActivity.this, LoginActivity.class);
-            startActivity(intent);
-
-        };
 
     }
+
 }
